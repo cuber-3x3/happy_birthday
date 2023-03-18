91c5d97e276c959b4a8713aaf8fa9d51a39444e9
cuber-3x3 <127592093+cuber-3x3@users.noreply.github.com>
2023-03-18 14:57:01 +0800

    add candles

--- main.ts
+++ main.ts
@@ -1,43 +1,66 @@
+function 点亮 () {
+    led.plot(1, 0)
+    led.plot(3, 0)
+}
 input.onLogoEvent(TouchButtonEvent.Touched, function () {
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
     for (let index = 0; index < 2; index++) {
         for (let index = 0; index < 1; index++) {
             music.playTone(262, music.beat(BeatFraction.Half))
             music.playTone(262, music.beat(BeatFraction.Half))
             music.playTone(294, music.beat(BeatFraction.Whole))
+            熄灭()
             music.playTone(262, music.beat(BeatFraction.Whole))
             music.playTone(349, music.beat(BeatFraction.Whole))
             music.playTone(330, music.beat(BeatFraction.Whole))
+            点亮()
             music.rest(music.beat(BeatFraction.Half))
         }
         for (let index = 0; index < 1; index++) {
             music.playTone(262, music.beat(BeatFraction.Half))
             music.playTone(262, music.beat(BeatFraction.Half))
             music.playTone(294, music.beat(BeatFraction.Whole))
+            熄灭()
             music.playTone(262, music.beat(BeatFraction.Whole))
             music.playTone(392, music.beat(BeatFraction.Whole))
             music.playTone(349, music.beat(BeatFraction.Whole))
+            点亮()
             music.rest(music.beat(BeatFraction.Half))
         }
         for (let index = 0; index < 1; index++) {
             music.playTone(262, music.beat(BeatFraction.Half))
             music.playTone(262, music.beat(BeatFraction.Half))
             music.playTone(523, music.beat(BeatFraction.Whole))
+            熄灭()
             music.playTone(440, music.beat(BeatFraction.Whole))
             music.playTone(349, music.beat(BeatFraction.Whole))
             music.playTone(330, music.beat(BeatFraction.Whole))
             music.playTone(294, music.beat(BeatFraction.Whole))
+            点亮()
             music.rest(music.beat(BeatFraction.Half))
         }
         for (let index = 0; index < 1; index++) {
             music.playTone(466, music.beat(BeatFraction.Half))
             music.playTone(466, music.beat(BeatFraction.Half))
             music.playTone(440, music.beat(BeatFraction.Whole))
+            熄灭()
             music.playTone(349, music.beat(BeatFraction.Whole))
             music.playTone(392, music.beat(BeatFraction.Double))
             music.playTone(349, music.beat(BeatFraction.Breve))
+            点亮()
+            music.rest(music.beat(BeatFraction.Whole))
         }
-        music.rest(music.beat(BeatFraction.Whole))
     }
 })
-music.setTempo(113)
+function 熄灭 () {
+    led.unplot(1, 0)
+    led.unplot(3, 0)
+}
+music.setTempo(120)
 music.setBuiltInSpeakerEnabled(true)
